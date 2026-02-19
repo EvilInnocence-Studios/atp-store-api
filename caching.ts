6518aa07167204267ab8d1bf807f088a2c78204b
Andrew Wittrock <andy@wittrock.us>
2026-02-19 12:47:22 -0600

    Remove cache types

--- caching.ts
+++ caching.ts
@@ -1,6 +1,4 @@
-import { IBehavior } from "../core/cloudfront";
-
-export const storeCaching:IBehavior[] = [
+export const storeCaching = [
     {precedence: 0, pathPattern: "/product/*/file/*/download", cache: false},
     {precedence: 1, pathPattern: "/product*",                 cache: true },
     {precedence: 2, pathPattern: "/discount*",                cache: true },
