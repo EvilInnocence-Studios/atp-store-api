aa60ec1e5cc194e92827521242dc1437a1b2e87b
Andrew Wittrock <andy@wittrock.us>
2026-05-17 14:17:22 -0500

    Add FieldRegistry to only allow some fields to be updated via the API

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { registerPermissionPlugin } from "../uac/permission/registry";
 import { init } from "../store/migrations/00-init";
 import { productPermissionPlugin } from "./product/permissionPlugin";
+import { FieldRegistry } from "@core/express/util";
 
 registerPermissionPlugin(productPermissionPlugin);
 
@@ -8,3 +9,23 @@ export { apiConfig } from "./endpoints";
 
 export const migrations = [init];
 export const setupMigrations = [init];
+
+FieldRegistry.register(
+    "products",
+    [
+        "name", "url", "sku", "price", "discount", "enabled", "pinned",
+        "metaTitle", "metaDescription", "metaKeywords"
+    ]
+);
+FieldRegistry.register("productFiles", ["fileName", "folder"]);
+FieldRegistry.register("productMedia", ["url", "caption", "order"]);
+FieldRegistry.register("subProducts", []);
+FieldRegistry.register("wishlists", []);
+FieldRegistry.register("relatedProducts", []);
+FieldRegistry.register("productTags", []);
+FieldRegistry.register("discounts", ["name", "type", "amount", "couponCode", "permissionId"]);
+FieldRegistry.register(
+    "orders",
+    ["total", "subtotal", "discount", "couponCode", "status", "transactionId", "createdAt"]
+);
+FieldRegistry.register("orderLineItems", ["quantity"]);
