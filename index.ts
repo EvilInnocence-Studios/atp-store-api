042df9686ba7e03e279cd6e1986e2d4efe6dd6eb
Andrew Wittrock <andy@wittrock.us>
2026-05-19 13:37:55 -0500

    Add whitelisted create fields

--- index.ts
+++ index.ts
@@ -11,21 +11,50 @@ export const migrations = [init];
 export const setupMigrations = [init];
 
 FieldRegistry.register(
-    "products",
-    [
-        "name", "url", "sku", "price", "discount", "enabled", "pinned",
-        "metaTitle", "metaDescription", "metaKeywords"
-    ]
-);
-FieldRegistry.register("productFiles", ["fileName", "folder"]);
-FieldRegistry.register("productMedia", ["url", "caption", "order"]);
-FieldRegistry.register("subProducts", []);
-FieldRegistry.register("wishlists", []);
-FieldRegistry.register("relatedProducts", []);
-FieldRegistry.register("productTags", []);
-FieldRegistry.register("discounts", ["name", "type", "amount", "couponCode", "permissionId"]);
+    "products", {
+    create: [
+        "name", "sku", "url", "description", "descriptionShort", "productType", "releaseDate", "price", "enabled", "pinned", "metaTitle", "metaDescription", "metaKeywords", "thumbnailId", "mainImageId", "isDiscountable"
+    ],
+    update: [
+        "name", "sku", "url", "description", "descriptionShort", "productType", "releaseDate", "price", "enabled", "pinned", "metaTitle", "metaDescription", "metaKeywords", "thumbnailId", "mainImageId", "isDiscountable"
+    ],
+});
+FieldRegistry.register("productFiles", {
+    create: ["productId", "fileName", "folder"],
+    update: ["fileName", "folder"],
+});
+FieldRegistry.register("productMedia", {
+    create: ["productId", "url", "caption", "order"],
+    update: ["url", "caption", "order"],
+});
+FieldRegistry.register("subProducts", {
+    create: ["productId", "subProductId"],
+    update: [],
+});
+FieldRegistry.register("wishlists", {
+    create: ["productId", "userId"],
+    update: [],
+});
+FieldRegistry.register("relatedProducts", {
+    create: ["productId", "relatedProductId"],
+    update: [],
+});
+FieldRegistry.register("productTags", {
+    create: ["productId", "tagId"],
+    update: [],
+});
+FieldRegistry.register("discounts", {
+    create: ["name", "type", "amount", "couponCode", "permissionId"],
+    update: ["name", "type", "amount", "couponCode", "permissionId"],
+});
 FieldRegistry.register(
     "orders",
-    ["total", "subtotal", "discount", "couponCode", "status", "transactionId", "createdAt"]
+    {
+        create: ["userId", "total", "subtotal", "discount", "couponCode", "status", "transactionId", "createdAt"],
+        update: ["total", "subtotal", "discount", "couponCode", "status", "transactionId", "createdAt"],
+    }
 );
-FieldRegistry.register("orderLineItems", ["quantity"]);
+FieldRegistry.register("orderLineItems", {
+    create: ["productId", "orderId", "quantity"],
+    update: ["quantity"],
+});
